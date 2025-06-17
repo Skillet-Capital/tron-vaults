11d3526be1bf58735008ec1d83676e7a60a031ff
Andrew Warner <warneracw@gmail.com>
2025-06-17 17:59:08 -0400

    fee recipient (#1)

--- contracts/Vault.sol
+++ contracts/Vault.sol
@@ -14,7 +14,16 @@ contract Vault {
     address public immutable owner;
     uint256 public nonce;
 
-    event TokenSent(address indexed token, address indexed to, uint256 amount);
+    event TokenSent(
+        address indexed token,
+        address indexed to,
+        uint256 amount,
+        address feeRecipient,
+        uint256 fee,
+        uint256 deadline,
+        uint256 nonce
+    );
+
     event VaultNonceIncremented(uint256 indexed nonce);
 
     /// @notice Initializes the vault with the owner's address and the USDT token
@@ -27,31 +36,49 @@ contract Vault {
     /// @param token Address of the token to be sent
     /// @param to Recipient address
     /// @param amount Amount of token to send
+    /// @param feeRecipient Fee recipient
+    /// @param fee Fee amount
+    /// @param deadline Signature deadline
     /// @param sig EIP-191-style signature from the `owner` authorizing the transfer
     function send(
         address token,
         address to,
         uint256 amount,
+        address feeRecipient,
+        uint256 fee,
         uint256 deadline,
         bytes calldata sig
     ) external {
         require(block.timestamp < deadline, "Vault: deadline exceeded");
+        require(fee <= amount, "Vault: fee exceeds amount");
+
+        if (fee > 0) {
+            require(feeRecipient != address(0), "Vault: invalid fee recipient");
+        }
 
         bytes32 messageHash = keccak256(
-            abi.encodePacked(token, to, amount, deadline, nonce)
+            abi.encodePacked(token, to, amount, feeRecipient, fee, deadline, nonce)
         );
 
         address recovered = _recover(messageHash, sig);
         require(recovered == owner, "Vault: invalid signature");
 
+        uint256 netAmount = amount - fee;
         require(
-            ITRC20(token).transfer(to, amount),
+            ITRC20(token).transfer(to, netAmount),
             "Vault: Token transfer failed"
         );
 
+        if (fee > 0) {
+            require(
+                ITRC20(token).transfer(feeRecipient, fee),
+                "Vault: Token transfer failed"
+            );
+        }
+
         nonce++;
 
-        emit TokenSent(token, to, amount);
+        emit TokenSent(token, to, amount, feeRecipient, fee, deadline, nonce);
         emit VaultNonceIncremented(nonce);
     }
 
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,7 +2,7 @@ const VaultFactory = artifacts.require('./VaultFactory.sol');
 const EntryPoint = artifacts.require('./EntryPoint.sol');
 
 module.exports = function (deployer) {
-  deployer.deploy(VaultFactory).then(() => {
-    return deployer.deploy(EntryPoint, VaultFactory.address);
-  });
+  // deployer.deploy(VaultFactory).then(() => {
+  //   return deployer.deploy(EntryPoint, VaultFactory.address);
+  // });
 };
